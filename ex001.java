ac86919efc4c4386b08023fbfedbc2a9294660cb
Елизавета Синявская <am8nl1492@gmail.com>
2023-04-04 18:17:53 +0300

    ex001.java

--- ex001.java
+++ ex001.java
@@ -1,37 +1,40 @@
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.lang.System.Logger;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-
 public class ex001 {
   static Logger logger;
+
   public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {
 
-    WriteJsonToFile();
-
-  }
-
-  static void WriteJsonToFile() throws FileNotFoundException, IOException, ParseException {
-    
-    //получние json строки
-    String file1 = "ex001.json";
+    // получние json строки
+    String file1 = "JsonData.json";
     String s1 = GetJsonData(file1);
 
-    //запись в файл
-    String file2 = "ex001.txt";
-    WriteToFile(s1, file2);
+    // запись фразы json в файл
+    String file2 = "JsonPhraseToFile.txt";
+    WriteJsonPhraseToFile(s1, file2);
+
+    String file3 = "LoggerJsonPhraseToFile.txt";
+    WriteToLoggerFile(s1, file3);
+
+    String file4 = "SortArr.txt";
+    SortArr(file4);
 
   }
 
-  static String GetJsonData(String file1) throws FileNotFoundException, IOException, ParseException {
-    
-    Object o = new JSONParser().parse(new FileReader(file1));
+  static String GetJsonData(String file) throws FileNotFoundException, IOException, ParseException {
+
+    Object o = new JSONParser().parse(new FileReader(file));
 
     JSONObject j = (JSONObject) o;
 
@@ -52,33 +55,76 @@ public class ex001 {
 
   }
 
-  static void WriteToFile(String s1, String file2) {
-  
-    try (FileWriter writer = new FileWriter(file2, false)) {
-      writer.write(s1);
+  static void WriteJsonPhraseToFile(String s, String file) {
+
+    try (FileWriter writer = new FileWriter(file, false)) {
+      writer.write(s);
       writer.flush();
     } catch (Exception e) {
       e.printStackTrace();
     }
 
-  //   try {
-  //     Logger logger = Logger.getAnonymousLogger();
-  //     FileHandler fileHandler = new FileHandler("log.txt", true);
-  //     logger.addHandler(fileHandler);
-  //     SimpleFormatter formatter = new SimpleFormatter();
-  //     fileHandler.setFormatter(formatter);
-  //     try (FileWriter writer = new FileWriter(filePath, false)) {
-  //         writer.write(s);
-  //         writer.flush();
-  //         logger.log(Level.INFO, " ");
-  //     } catch (Exception e) {
-  //         e.printStackTrace();
-  //         logger.log(Level.WARNING, e.getMessage());
-  //     }
-  //     fileHandler.close();
-  // } catch (Exception e){
-  //     e.printStackTrace();
-  // }
+  }
+
+  static void WriteToLoggerFile(String s, String file) {
+
+    try {
+      Logger logger = Logger.getAnonymousLogger();
+      FileHandler fileHandler = new FileHandler("Logger.txt", true);
+      logger.addHandler(fileHandler);
+      try (FileWriter writer = new FileWriter(file, false)) {
+        writer.write(s);
+        writer.flush();
+        logger.log(Level.INFO, "запись успешно создана");
+      } catch (Exception e) {
+        e.printStackTrace();
+        logger.log(Level.WARNING, e.getMessage());
+      }
+      fileHandler.close();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+  }
+
+  static void SortArr(String file) {
+
+    int[] array = new int[] { 2, 3, 22, 1, 5, 9, 6, 1, 7 };
+
+    for (int i = 0; i < array.length; i++) {
+      System.out.print(array[i] + " ");
+    }
+    System.out.println("");
+
+    boolean isSorted = false;
+    while (!isSorted) {
+      isSorted = true;
+      for (int i = 1; i < array.length; i++) {
+        if (array[i] < array[i - 1]) {
+          int temp = array[i];
+          array[i] = array[i - 1];
+          array[i - 1] = temp;
+          isSorted = false;
+        }
+      }
+    }
+
+    String s = "";
+
+    for (int i = 0; i < array.length; i++) {
+      System.out.print(array[i] + " ");
+      s += array[i] + " ";
+    }
+
+    try {
+      Logger logger = Logger.getAnonymousLogger();
+      FileHandler fileHandler = new FileHandler("Logger2.txt", true);
+      logger.addHandler(fileHandler);
+      logger.log(Level.INFO, "массив отсортирован", s);
+      fileHandler.close();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
 
   }
 }
